6e531d07d14ce7c55f84c4325f53257d49d8a409
tjr44 <qlaxmansatyacharan369@gmail.com>
2025-10-31 16:26:36 +0530

    added exception handling

---
+++ ntime_date.py
@@ -0,0 +1,2 @@
+#  date : `31-Oct-2025`   module name :`run_m.py`   time :`>>> 04:09:23 PM`
+#  date : `31-Oct-2025`   module name :`run_m.py`   time :`>>> 04:12:14 PM`
--- run_m.py
+++ run_m.py
@@ -1,15 +1,17 @@
 #now work with functions note time
 import time
 import os
-import sys
 from typing import Any
+
+class ReqErr(Exception):
+        pass
 
 class ntime_code_name:
           def __init__(self,file_name = __file__):
-                    sys.path.append("C:/rdt_engine")
-                    '''
-                    varibles-Datamembers
-                    '''
+                '''
+                varibles-Datamembers
+                '''
+                try:
                     #record current time
                     rc_t = time.strftime('>>> %I:%M:%S %p')   #type(c_t_n) <class 'str'>
                     #record current date
@@ -20,7 +22,10 @@ class ntime_code_name:
                     self.module_name = os.path.basename(file_name)
                     #full path of module
                     self.module_path = file_name
-
+                    #this helps prevent default module_name
+                    if self.module_name == 'run_m.py':
+                            raise ReqErr("\033[91m you necessary to add \033[94m __file__\033[0m \033[91m variable onto \033[94m ntime_code_name class\033[0m")
+                    
                     #file store operation
                     #currect time note # add data without erase and trancate the existsing data a also, create file wihout Filenot found error if is not exists
                     with open('ntime_date.py','a') as ntdo:
@@ -32,6 +37,15 @@ class ntime_code_name:
                     #time record into ntime_date
                     print(f"\033[92m \n\t\t\tfilename and datetime was recored:) ---> \033[91m`{self.module_name}\033[0m` , \033[94m`{c_dt}`\033[0m \n\033[0m")
 
+                except ReqErr as request_error:
+                      "you must do enter __file__ Argument into the ntime_code_name class"
+                      print(f"Error is : {request_error}")
+                
+                except Exception:
+                       print("\033[91m Error :  Check...Code...please...check...code \033[0m")
+                finally:
+                  pass
+          
 #tri_underscore main function very important private method treadted by python 
 def ___main___() -> Any:
         n2c = ntime_code_name()
